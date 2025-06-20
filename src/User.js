519c772cff767a8cce9d7885f6753f9ef368358d
mfaiz2003 <mfaiz76542@gmail.com>
2025-06-20 23:12:14 +0530

    Initial commit

--- src/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  fullname: String,
-  contactno: { type: String, unique: true },
-  password: String
-});
-
-module.exports = mongoose.model('User', userSchema);
--- src/script.js
+++ src/script.js
@@ -2,10 +2,10 @@
   fetchDate('https://worldtimeapi.org/api/timezone/Etc/UTC')
   .then(response => response.json())
   .then(data => {
-    const fullDateTime = data.datetime; // Format: 2025-05-06T14:34:12.123456+00:00
-    const dateOnly = fullDateTime.split('T')[0]; // sirf date part nikalna (e.g. 2025-05-06)
+    const fullDateTime = data.datetime; 
+    const dateOnly = fullDateTime.split('T')[0]; 
 
-    // Input field me set karna
+    //
     document.getElementById('dateInput').value = dateOnly;
   })
   .catch(error => {
@@ -13,20 +13,16 @@
   });
 
   async function fetchTime() {
-    // Fetch current time from the WorldTime API
     const response = await fetch('http://worldtimeapi.org/api/timezone/Asia/Kolkata');
     const data = await response.json();
   
-    // Get the time from the API response
+    
     const datetime = data.datetime;
   
-    // Extract the time portion (HH:MM:SS)
-    const time = datetime.substring(11, 16); // HH:MM from 'YYYY-MM-DDTHH:MM:SS'
-  
-    // Set the value of the time input field
+    const time = datetime.substring(11, 16);
+
     document.getElementById('time').value = time;
   }
 
-  localStorage.setItem("userId", response.user._id);
 
  
